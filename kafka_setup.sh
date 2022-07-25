b14ed0f04289349c28f3664a57b554fbf7e8383f
nilkamal.thakuria <nilkamal.thakuria@bicycle.io>
2022-07-25 19:26:10 +0530

    Fix minor path issue

--- kafka_setup.sh
+++ kafka_setup.sh
@@ -4,7 +4,7 @@ echo "Installing strimzi kafka operator"
 helm repo add strimzi https://strimzi.io/charts/
 helm install strimzi-kafka-operator strimzi/strimzi-kafka-operator
 echo "Initiating Kafka Cluster using tirth-test-kafka"
-cd ./tirth-test-kafka
+cd ./Kafka_LT_Kubernetes/tirth-test-kafka
 helm install tirth-test-kafka .
-cd ..
+cd ${HOME}
 rm -rf Kafka_LT_Kubernetes
